a7f72bfb47759086f1422e1a48383ee1009053a3
Alejandro Román Franco <alejoroman0605@gmail.com>
2024-02-09 18:24:18 -0500

    Update EasyDNNNewsCategoryList.cs

--- Modules/HubSpotEasyDnnNewsBlogMigrator/Models/EasyDNNNewsCategoryList.cs
+++ Modules/HubSpotEasyDnnNewsBlogMigrator/Models/EasyDNNNewsCategoryList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace UpendoVentures.Modules.HubSpotEasyDnnNewsBlogMigrator.Models
 {
     public class EasyDNNNewsCategoryList
     {
+        [Key]
         public int CategoryID { get; set; }
         public int PortalID { get; set; }
         public string CategoryName { get; set; }
