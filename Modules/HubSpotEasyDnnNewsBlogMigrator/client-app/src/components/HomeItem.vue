c090e6ef7e0d712e0ebeb1bf303fcfe0a2dd42cc
Alejandro Román Franco <alejoroman0605@gmail.com>
2024-02-09 18:24:06 -0500

    Update HomeItem.vue

--- Modules/HubSpotEasyDnnNewsBlogMigrator/client-app/src/components/HomeItem.vue
+++ Modules/HubSpotEasyDnnNewsBlogMigrator/client-app/src/components/HomeItem.vue
@@ -3,10 +3,13 @@
         <div class="text-center">
             <h2>{{ resx.Welcome }}</h2>
             <ul class="dnnActions dnnClear">
-                <li><a class="dnnPrimaryAction" href=".">Go Ahead</a></li>
+                <li><a class="dnnPrimaryAction" @click="migratePosts()">Migrate Posts</a></li>
             </ul>
             <ul class="dnnActions dnnClear">
                 <li><a class="dnnPrimaryAction" @click="updateSettings()">Update Settings</a></li>
+            </ul>
+            <ul class="dnnActions dnnClear">
+                <li><a class="dnnPrimaryAction" @click="geturlForInitiateOAuth()">Initiate OAuth</a></li>
             </ul>
         </div>
     </div>
@@ -14,8 +17,8 @@
 
 <script setup>
 import { inject, ref, } from 'vue';
-import axios from "axios";
-import { getUrlBase } from "../assets/utils";
+import { makeRequest } from '../assets/api.js';
+import { getCookie } from '../assets/utils.js';
 
 // Injected dependencies
 const resx = inject("resx");
@@ -23,116 +26,70 @@ const dnnConfig = inject("dnnConfig");
 
 // Reactive references
 const items = ref([]);
-var baseUrl = `${getUrlBase("Hubspot")}`;
 
 // Variables
-let accessToken = "CIGA18zYMRICAgEYturAFSCF6aceKKi-rgEyFHChQXlu9_fXOKdhOq_pzj8s9xTJOj8AIABB_wcAAAAAgAAAYHjAIAAAAAAAAAAEAAAYAAAAwMMfAAEAAACABgAAAAAAAAAAAAAAAAAAAAAAAgAIuAJCFNgc1RTjgBQdXFXYVb3qn66FuII_SgNuYTFSAFoA"
-// Functions
-const fetchItems = async () => {
-    var url = `${baseUrl}/GetBlogPosts`;
-    let axiosConfig = {
-        url: url,
-        headers: {
-            'Content-Type': 'application/json',
-            moduleid: dnnConfig.moduleId,
-            tabid: dnnConfig.tabId,
-            RequestVerificationToken: dnnConfig.rvt,
-            AccessToken: accessToken,
-        },
-        withCredentials: true
-    };
-    axios({
-        ...axiosConfig
-    }).then((response) => {
-        if (response.status === 200) {
-            items.value = response.data;
-        }
-    }).catch((error) => {
-        console.log(error);
-    });
-};
+const accessToken = getCookie('access_token');
 
-const migratePosts = async () => {
-    var url = `${baseUrl}/MigratePosts`;
-    let axiosConfig = {
-        url: url,
-        headers: {
-            'Content-Type': 'application/json',
-            moduleid: dnnConfig.moduleId,
-            tabid: dnnConfig.tabId,
-            RequestVerificationToken: dnnConfig.rvt,
-            AccessToken: accessToken,
-        },
-        withCredentials: true
-    };
-    axios({
-        ...axiosConfig
-    }).then((response) => {
-        if (response.status === 200) {
-            items.value = response.data;
+// Functions
+const checkCode = async () => {
+    const url = new URL(window.location.href);
+    const params = new URLSearchParams(url.search);
+    const code = params.get('code');
+    if (code) {
+        const result = await makeRequest(dnnConfig, 'OAuthCallback', 'post', code, accessToken);
+        console.log("result", result);
+        if (result.access_token != null) {
+            document.cookie = `access_token=${result.access_token}; path=/`;
         }
-    }).catch((error) => {
-        console.log(error);
-    });
-};
+        // else {
+        //     document.cookie = "access_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        // }
+        const url = new URL(window.location.href);
+        window.location.href = url.origin + url.pathname;
+    }
+}
+
+const fetchItems = async () => {
+    const result = await makeRequest(dnnConfig, 'GetBlogPosts', 'get', null, accessToken);
+    if (result) {
+        items.value = result;
+    }
+}
 
 const getSettings = async () => {
-    var url = `${baseUrl}/GetSettings`;
-    let axiosConfig = {
-        url: url,
-        headers: {
-            'Content-Type': 'application/json',
-            moduleid: dnnConfig.moduleId,
-            tabid: dnnConfig.tabId,
-            RequestVerificationToken: dnnConfig.rvt,
-            AccessToken: accessToken,
-        },
-        withCredentials: true
-    };
-    axios({
-        ...axiosConfig
-    }).then((response) => {
-        if (response.status === 200) {
-            items.value = response.data;
-        }
-    }).catch((error) => {
-        console.log(error);
-    });
-};
+    const result = await makeRequest(dnnConfig, 'GetSettings', 'get', null, accessToken);
+    if (result) {
+        items.value = result;
+    }
+}
 
-function updateSettings() {
+const updateSettings = async () => {
     var data = {
         ClientId: "e7d75ea8-f8d6-416e-9817-7e3a00b00b82",
         ClientSecret: "cd7713a0-14ce-4a9d-87a5-3c156caf3f6f",
         RedirectUri: "https://localhost.dnndev.me/hubspot",
         Scope: "content"
     }
-    var url = `${baseUrl}/UpdateSettings`;
-    let axiosConfig = {
-        method: 'post',
-        url: url,
-        data: data,
-        headers: {
-            'Content-Type': 'application/json',
-            moduleid: dnnConfig.moduleId,
-            tabid: dnnConfig.tabId,
-            RequestVerificationToken: dnnConfig.rvt
-        },
-        withCredentials: true,
-    };
-    axios({
-        ...axiosConfig
-    }).then((response) => {
-        if (response.status === 200) {
-            fetchItems();
-        }
-    }).catch((error) => {
-        console.log(error);
-    });
+    const response = await makeRequest(dnnConfig, 'UpdateSettings', 'post', data);
+    if (response) {
+        fetchItems();
+    }
+}
+
+async function geturlForInitiateOAuth() {
+    var url = await makeRequest(dnnConfig, 'InitiateOAuth');
+    window.open(url, '_blank');
+}
+
+async function migratePosts() {
+    const result = await makeRequest(dnnConfig, 'MigratePosts', 'get', null, accessToken);
+    if (result) {
+        items.value = result;
+    }
 }
 
 // Executed methods during the component's mounting phase
+checkCode(); // Check for code in the URL
 fetchItems();// Initial fetching of items
-migratePosts();
-getSettings();
+getSettings(); // Get settings
 </script>
