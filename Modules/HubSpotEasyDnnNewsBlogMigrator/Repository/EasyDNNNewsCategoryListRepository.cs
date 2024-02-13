783f046e3b2bc014fdf5af18b8f4f6451e9838fa
Alejandro Román Franco <alejoroman0605@gmail.com>
2024-02-13 09:21:33 -0500

    Update EasyDNNNewsCategoryListRepository.cs

--- Modules/HubSpotEasyDnnNewsBlogMigrator/Repository/EasyDNNNewsCategoryListRepository.cs
+++ Modules/HubSpotEasyDnnNewsBlogMigrator/Repository/EasyDNNNewsCategoryListRepository.cs
@@ -24,22 +24,11 @@ namespace UpendoVentures.Modules.HubSpotEasyDnnNewsBlogMigrator.Repository
     {
         private readonly IDbConnection _connection;
         private readonly ILog _logger;
-        private readonly ModuleController _moduleController;
-        private readonly int _portalId;
-        private readonly ModuleInfo _module;
-        private readonly UserInfo _currentUser;
-        private readonly IEncryptionHelper _encryptionHelper;
-        private readonly string ResourceFile = Constant.ResxRoot;
-
-        public EasyDNNNewsCategoryListRepository(DapperContext context, IEncryptionHelper encryptionHelper) : base(context)
+     
+        public EasyDNNNewsCategoryListRepository(DapperContext context) : base(context)
         {
             _connection = context.CreateConnection();
             _logger = LoggerSource.Instance.GetLogger(GetType());
-            _moduleController = new ModuleController();
-            _portalId = PortalController.Instance.GetCurrentPortalSettings().PortalId;
-            _module = _moduleController.GetModuleByDefinition(_portalId, Constant.FriendlyName);
-            _currentUser = UserController.Instance.GetCurrentUserInfo();
-            _encryptionHelper = encryptionHelper;
         }
 
         /// <summary>
