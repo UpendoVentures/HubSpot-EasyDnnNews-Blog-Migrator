cfae81697b5dc2c765cad72e7905df875a240212
Alejandro Román Franco <alejoroman0605@gmail.com>
2024-02-28 14:53:44 -0500

    Create EasyDNNNewsTagsItems.cs

---
+++ Modules/HubSpotEasyDnnNewsBlogMigrator/Models/EasyDNNNewsTagsItems.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UpendoVentures.Modules.HubSpotEasyDnnNewsBlogMigrator.Models
+{
+    public class EasyDNNNewsTagsItems
+    {
+        public int ArticleID { get; set; }
+        public long TagID { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
