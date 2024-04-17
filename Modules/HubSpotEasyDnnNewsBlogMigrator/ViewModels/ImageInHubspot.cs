d7645eee93ea7637d2f350867e1e7f227176bf1f
Alejandro Román Franco <alejoroman0605@gmail.com>
2024-04-17 09:19:19 -0400

    Create ImageInHubspot.cs

---
+++ Modules/HubSpotEasyDnnNewsBlogMigrator/ViewModels/ImageInHubspot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UpendoVentures.Modules.HubSpotEasyDnnNewsBlogMigrator.ViewModels
+{
+    public class ImageInHubspot
+    {
+        public string ImageSRC { get; set; }
+        public string ImageArticle { get; set; }
+        public int ArticleId { get; set; }
+    }
+}
